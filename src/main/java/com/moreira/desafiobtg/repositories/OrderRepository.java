9277d92bca4d91211f40bd678c415493b8ae9e89
zenonxd <olavomoreira99@gmail.com>
2024-12-01 00:25:51 -0300

    feat: repositorie and service layer

---
+++ src/main/java/com/moreira/desafiobtg/repositories/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.moreira.desafiobtg.repositories;
+
+import com.moreira.desafiobtg.entities.Order;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends MongoRepository<Order, Long> {
+}
---
+++ src/main/java/com/moreira/desafiobtg/services/OrderService.java
@@ -0,0 +1,55 @@
+package com.moreira.desafiobtg.services;
+
+import com.moreira.desafiobtg.dtos.OrderCreatedResponse;
+import com.moreira.desafiobtg.dtos.OrderItemDto;
+import com.moreira.desafiobtg.entities.Order;
+import com.moreira.desafiobtg.entities.OrderItem;
+import com.moreira.desafiobtg.repositories.OrderRepository;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.stream.Collectors;
+
+@Service
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+
+    public OrderService(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
+    private static OrderItem getOrderItems(OrderItemDto item) {
+        return new OrderItem(
+                item.produto(),
+                item.quantidade(),
+                item.preco()
+        );
+    }
+
+    public void save(OrderCreatedResponse event) {
+        Order order = new Order();
+        order.setOrderId(event.codigoPedido());
+        order.setClientId(event.codigoCliente());
+
+        order.setTotal(getTotal(event));
+
+
+        order.setOrderItems(
+                event.itens()
+                        .stream()
+                        .map(OrderService::getOrderItems)
+                .toList());
+
+        orderRepository.save(order);
+    }
+
+    private BigDecimal getTotal(OrderCreatedResponse event) {
+        return event.itens()
+                .stream()
+                .map(i -> i.preco().multiply(BigDecimal.valueOf(i.quantidade())))
+                .reduce(BigDecimal::add)
+                //caso não tenha nada retorna um zero
+                .orElse(BigDecimal.ZERO);
+    }
+}
