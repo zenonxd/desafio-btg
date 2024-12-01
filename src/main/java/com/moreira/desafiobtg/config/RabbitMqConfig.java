ddfe4f896b93f38455cc2e6bc8503bd4053c16ae
zenonxd <olavomoreira99@gmail.com>
2024-12-01 00:26:07 -0300

    feat: rabbitMQ config (queue and json converter)

---
+++ src/main/java/com/moreira/desafiobtg/config/RabbitMqConfig.java
@@ -0,0 +1,28 @@
+package com.moreira.desafiobtg.config;
+
+
+import org.springframework.amqp.core.Declarable;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMqConfig {
+
+    public static final String ORDER_CREATED_QUEUE = "btg-pactual-order-created";
+
+
+    //convertindo from JSON to Java
+    @Bean
+    public Jackson2JsonMessageConverter jackson2JsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+
+    //creating the queue
+    @Bean
+    public Declarable orderCreatedQueue() {
+        return new Queue(ORDER_CREATED_QUEUE);
+    }
+}
