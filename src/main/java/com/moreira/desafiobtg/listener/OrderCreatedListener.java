544805cbaaf55904fc80320f6d0594492cfd4022
zenonxd <olavomoreira99@gmail.com>
2024-12-01 00:25:25 -0300

    feat: creating RabbitMQ listener

---
+++ src/main/java/com/moreira/desafiobtg/listener/OrderCreatedListener.java
@@ -0,0 +1,30 @@
+package com.moreira.desafiobtg.listener;
+
+import com.moreira.desafiobtg.dtos.OrderCreatedResponse;
+import com.moreira.desafiobtg.services.OrderService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.messaging.Message;
+import org.springframework.stereotype.Component;
+
+import static com.moreira.desafiobtg.config.RabbitMqConfig.ORDER_CREATED_QUEUE;
+
+@Component
+public class OrderCreatedListener {
+
+    private final Logger logger = LoggerFactory.getLogger(OrderCreatedListener.class);
+
+    private final OrderService orderService;
+
+    public OrderCreatedListener(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @RabbitListener(queues = ORDER_CREATED_QUEUE)
+    public void listen(Message<OrderCreatedResponse> message) {
+        logger.info("Message consumed: {}", message);
+
+        orderService.save(message.getPayload());
+    }
+}
